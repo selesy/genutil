1389d05aad98cf3346429ef1080d7ce273d84fb9
Steve Moyer <smoyer@psu.edu>
2019-02-14 16:46:00 -0500

    Copied argument management from embedded project

---
+++ genutil/arguments.go
@@ -0,0 +1,23 @@
+package genutil
+
+import (
+	"os"
+	"strings"
+)
+
+func App() (app string) {
+	return os.Args[0]
+}
+
+func NonFlagArgs() (args []string) {
+	return nonFlagArgs(os.Args[1:])
+}
+
+func nonFlagArgs(argsIn []string) (argsOut []string) {
+	for _, argIn := range argsIn {
+		if !strings.HasPrefix(argIn, "-") {
+			argsOut = append(argsOut, argIn)
+		}
+	}
+	return
+}
