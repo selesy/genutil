e3565cec2a529c19101f3831a25ad2b0bd94f32e
Steve Moyer <smoyer@psu.edu>
2019-03-11 16:55:19 -0400

    Removes the enumeration-like test

--- genutil/filter_test.go
+++ genutil/filter_test.go
@@ -1,10 +1,7 @@
 package genutil
 
 import (
-	"fmt"
 	"go/ast"
-	"go/token"
-	"reflect"
 	"testing"
 
 	log "github.com/sirupsen/logrus"
@@ -45,64 +42,7 @@ func TestStructFiltered(t *testing.T) {
 	assert := assert.New(t)
 
 	structArrayFilter := func(node ast.Node) bool {
-		fmt.Println("-----")
-
-		//_, ok := node.(*ast.ValueSpec)
-		genDecl, ok := node.(*ast.GenDecl)
-		if !ok {
-			return ok
-		}
-		if genDecl.Tok != token.VAR {
-			return false
-		}
-
-		fmt.Printf("     GenDecl: %v\n", genDecl)
-		fmt.Printf("     GenDecl CG: %v\n", genDecl.Doc)
-
-		if len(genDecl.Specs) != 1 {
-			return false
-		}
-		valueSpec, ok := genDecl.Specs[0].(*ast.ValueSpec)
-		if !ok {
-			return ok
-		}
-		fmt.Printf("     Spec: %v\n", valueSpec)
-
-		compLit, ok := valueSpec.Values[0].(*ast.CompositeLit)
-		if !ok {
-			return false
-		}
-		fmt.Println("     CompositeLit: ", compLit)
-		fmt.Println("     CompositeLit type: ", compLit.Type)
-		fmt.Println("     CompositeLit type: ", reflect.TypeOf(compLit.Type))
-		arrayType, ok := compLit.Type.(*ast.ArrayType)
-		if !ok {
-			return false
-		}
-		fmt.Println("     ArrayType:", arrayType)
-		fmt.Println("     ArrayType element:", arrayType.Elt)
-		arrayStructType, ok := arrayType.Elt.(*ast.StructType)
-		if !ok {
-			return false
-		}
-		fmt.Println("     ", arrayStructType)
-		for _, field := range arrayStructType.Fields.List {
-			fmt.Println("     ArrayStruct field:", field)
-		}
-
-		for _, compLitElt := range compLit.Elts {
-			fmt.Println("     CompositeLit element: ", compLitElt)
-			fmt.Println("     CompositeLit element type: ", reflect.TypeOf(compLitElt))
-			innerCompLit, ok := compLitElt.(*ast.CompositeLit)
-			if !ok {
-				return false
-			}
-			for _, innerElt := range innerCompLit.Elts {
-				fmt.Println("     Inner element:", innerElt)
-			}
-		}
-
-		return ok
+		return true //TODO - filter some of the incoming nodes
 	}
 
 	nodes, err := FilterAstNodesFromPatterns(structArrayFilter, "../_example")
@@ -110,5 +50,5 @@ func TestStructFiltered(t *testing.T) {
 		log.Error(err)
 		assert.Error(err)
 	}
-	assert.Len(nodes, 1)
+	assert.Len(nodes, 8) //TODO - see above
 }
